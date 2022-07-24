96b7c56cea07988cf0433ca69a951305bc233b07
Aziz Gainullin <gaynullin_aziz1990@mail.ru>
2022-07-24 16:27:24 +0300

    4th version

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -19,11 +19,20 @@ class Post(models.Model):
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts'
+        blank=True,
+        null=True,
+        related_name='posts',
     )
     group = models.ForeignKey(
         Group,
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
         blank=True,
-        null=True
+        null=True,
+        related_name='posts',
     )
+
+class Meta:
+    ordering = ("-pub_date",)
+
+    def __str__(self):
+        return self.text    
