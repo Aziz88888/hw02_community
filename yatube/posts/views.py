90d10a953c0e1eeae6a7e81bc3879eae28207b8c
Aziz Gainullin <gaynullin_aziz1990@mail.ru>
2022-08-06 20:26:38 +0300

    version 1

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,20 +1,64 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Group
+from django.core.paginator import Paginator
+from .models import Post, Group, User
+from django.conf import settings
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    template = 'posts/index.html'
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
-    return render(request, 'posts/index.html', context)
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
+    template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
     posts = group.posts.all()[:10]
     context = {
         'group': group,
         'posts': posts,
     }
-    return render(request, 'posts/group_list.html', context)
+    return render(request, template, context)
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    posts = author.posts.all()
+    count = author.posts.count()
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    context = {
+        "page_obj": page_obj,
+        "count": count,
+        "author": author,
+    }
+
+    template = "posts/profile.html"
+
+    return render(request, template, context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    pub_date = post.pub_date
+    post_title = post.text[:30]
+    author = post.author
+    author_posts = author.posts.all().count()
+    context = {
+        "post": post,
+        "post_title": post_title,
+        "author": author,
+        "author_posts": author_posts,
+        "pub_date": pub_date,
+    }
+
+    template = "posts/post_detail.html"
+
+    return render(request, template, context)
