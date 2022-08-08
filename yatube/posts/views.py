8f8e22b1a4d8e4aca064c1b9dc8c31d2a811372d
Aziz Gainullin <gaynullin_aziz1990@mail.ru>
2022-08-08 17:39:05 +0300

    version 2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
 from .models import Post, Group, User
-from django.conf import settings
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -62,3 +63,40 @@ def post_detail(request, post_id):
     template = "posts/post_detail.html"
 
     return render(request, template, context)
+
+
+@login_required
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect(f'/profile/{post.author}/', {'form': form})
+    form = PostForm()
+    groups = Group.objects.all()
+    template = 'posts/create_post.html'
+    context = {'form': form, 'groups': groups}
+    return render(request, template, context)
+
+
+def post_edit(request, post_id):
+    is_edit = True
+    post = get_object_or_404(Post, pk=post_id)
+    author = post.author
+    groups = Group.objects.all()
+    form = PostForm(request.POST or None, instance=post)
+    template = "posts/create_post.html"
+    if request.user == author:
+        if request.method == "POST" and form.is_valid:
+            post = form.save()
+            return redirect("posts:post_detail", post_id)
+        context = {
+            "form": form,
+            "is_edit": is_edit,
+            "post": post,
+            "groups": groups,
+        }
+        return render(request, template, context)
+    return redirect("posts:post_detail", post_id)  
